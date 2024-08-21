07d99cc58847654b4cef59d3e05c520425c356de
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-08-21 00:10:30 -0700

    Implement Gameboard function

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // Gameboard Module
-    // Empty 3x3 gameboard
+const Gameboard = (function() {
+    let board = ["", "", "", "", "", "", "", "", ""]; // Empty 3x3 gameboard
+    
+    const getBoard = () => board;
+
+    const placeMarker = (index, marker) => {
+        if (board[index] === "") {
+            board[index] = marker;
+            return true;
+        }
+    };
+})();
+
+// Player Module
 
 // Game Controller Module
     // Gebo Rune for player 1
