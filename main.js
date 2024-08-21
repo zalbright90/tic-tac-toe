ea30aeeabf39dc40486b9943e53dbd09a6441f84
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-08-21 00:05:43 -0700

    Pseudocode for main concept and structure

---
+++ main.js
@@ -0,0 +1,22 @@
+// Gameboard Module
+    // Empty 3x3 gameboard
+
+// Game Controller Module
+    // Gebo Rune for player 1
+    // Ingwaz Rune for player 2
+
+    // Switch turns between players
+
+    // Current player
+
+    // Play turn
+
+    // Check for win conditions
+
+    // Check for tie conditions
+
+    // Display game state
+
+
+
+
